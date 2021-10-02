6cab7b327d9794015de3852d4d0e7b44094c1ad1
Hani <57943681+Hani311@users.noreply.github.com>
2021-10-02 12:42:42 +0200

    delete useless btns

--- app/src/main/java/com/example/homedork/MainActivity.java
+++ app/src/main/java/com/example/homedork/MainActivity.java
@@ -16,22 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.button);
-        Button btn2 = findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RegisterUser.class);
-                startActivity(intent);
-            }
-        });
     }
 }
