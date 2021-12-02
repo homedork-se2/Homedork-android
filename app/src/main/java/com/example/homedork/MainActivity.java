9e5f7e95699070f901ee76ce1affd55d944facfc
abdeen <ahmedabdeen11.freelancer@gmail.com>
2021-12-02 11:50:30 +0200

    - Added Toast

--- app/src/main/java/com/example/homedork/MainActivity.java
+++ app/src/main/java/com/example/homedork/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.homedork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.homedork.api.user.api.UserRequests;
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Main Activity", Toast.LENGTH_SHORT).show();
 
     }
 }
