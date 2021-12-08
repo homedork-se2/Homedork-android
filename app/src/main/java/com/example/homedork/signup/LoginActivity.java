10627c10cbb404c7f315ed21962ec0c1b894f206
Hani <57943681+Hani311@users.noreply.github.com>
2021-12-08 18:25:27 +0100

    Add webview for the game

--- app/src/main/java/com/example/homedork/signup/LoginActivity.java
+++ app/src/main/java/com/example/homedork/signup/LoginActivity.java
@@ -14,10 +14,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.homedork.Dashbord2;
-import com.example.homedork.MainActivity;
 import com.example.homedork.R;
 import com.example.homedork.api.lamp.api.LampRequests;
-import com.example.homedork.test;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
